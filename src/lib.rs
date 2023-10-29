a6b03a9e32e85e2e7f3a793746e633f8402cd8cc
Liam Naddell <liamnprg@gmail.com>
2023-10-29 14:52:22 -0400

    add example for x509

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 use openssl::rsa::{Rsa,Padding};
 use openssl::pkey::Private;
+use openssl::x509::X509ReqBuilder;
+use openssl::hash::MessageDigest;
+use openssl::pkey::PKey;
+use openssl::x509::X509Req;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -29,5 +33,23 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+    #[test]
+    fn x509_demo() {
+        let verifier_rsa: Rsa<Private> = generate();
+        let verifier_pkey = PKey::from_rsa(verifier_rsa).unwrap();
+        //an x509 request is the certificate of the CA
+        let mut req_builder: X509ReqBuilder = X509ReqBuilder::new().unwrap();
+        req_builder.set_pubkey(&verifier_pkey);
+        let verifier_req: X509Req = req_builder.build();
+        //use to_pem to serialize
+
+        //Now we are going to use verifier_req to sign a certificate
+        let verified_rsa = generate();
+        let verified_pkey = PKey::from_rsa(verified_rsa).unwrap();
+        let mut other_builder = X509ReqBuilder::new().unwrap();
+        other_builder.set_pubkey(&verified_pkey);
+        other_builder.sign(&verifier_pkey,MessageDigest::sha384()).unwrap();
+        let verified_req: X509Req = other_builder.build();
     }
 }
