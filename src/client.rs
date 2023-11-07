b82eecfc9870f7c2250adb5b5aa0531747b011ec
Liam Naddell <liamnprg@gmail.com>
2023-11-07 15:25:06 -0500

    Add Updates to the protocol specification+demo code (#2)
    * Add more proto stuff
    
    * add update, updateresponse, doesn't seem to work

--- src/client.rs
+++ src/client.rs
@@ -1,36 +1,37 @@
 use cachain::*;
-use std::net::{SocketAddr, ToSocketAddrs,TcpStream};
-use std::str::FromStr;
+use std::net::{ToSocketAddrs,TcpStream};
 use capnp::serialize;
 use std::io::Write;
+use std::error::Error;
 
-const trusted_peers: [&str;1] = ["127.0.0.1:6969"];
-const my_addr: u64 = 10203;
+const TRUSTED_PEERS: [&str;1] = ["127.0.0.1:6969"];
+const MY_ADDR: u64 = 10203;
 
-fn main() {
+fn main() -> Result<(),Box<dyn Error>> {
     let db = load_db("client_db.json");
-    for peer in trusted_peers {
+    for peer in TRUSTED_PEERS {
         let mut addrs = peer.to_socket_addrs().unwrap();
         let mut socket = addrs.next().unwrap();
         socket.set_port(6969);
         println!("Connecting to peer {} with ip {:?}",peer,socket);
         let mut stream = TcpStream::connect(socket).unwrap();
         //send ping
-        let ping = create_ping(my_addr,0);
+        let ping = create_ping(MY_ADDR,0,&db.pkey);
 
-        stream.write(&ping);
+        stream.write(&ping)?;
 
-        let reader = serialize::read_message(stream,capnp::message::ReaderOptions::new()).unwrap();
+        let reader = serialize::read_message(&stream,capnp::message::ReaderOptions::new()).unwrap();
 
+        //TODO: add pong info to running list of peers
         let pong = reader.get_root::<msg_capnp::pong::Reader>().unwrap();
-
         println!("{:?}",pong);
+        deserialize_pubkey(pong.get_key()?.to_string()?);
 
+        let update = create_update(MY_ADDR,pong.get_src(),0,0);
 
-
-
-
-        //wait for pong
+        stream.write(&update)?;
+        let update_response = reader.get_root::<msg_capnp::update_response::Reader>()?;
+        println!("{:?}", update_response);
     }
-    println!("Hello World {}",add(1,2));
+    return Ok(());
 }
